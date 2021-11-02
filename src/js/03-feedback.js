f96688665af1b4be4b5623d4af2fea58fc3032ba
OlenaYarotska <olena.zhurba@gmail.com>
2021-11-02 11:36:26 +0200

    add fixes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,46 +4,33 @@ const FORM_SAVED_INF = 'formSavedInf';
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('.feedback-form input');
 const textarea = document.querySelector('.feedback-form textarea');
-const dataUpdate = {};
 
 form.addEventListener('submit', formSubmit);
 form.addEventListener('input', throttle(formInput, 500));
 
+const dataUpdate = {};
+filledTextarea();
+
+
 function formInput(e) {
     dataUpdate[e.target.name] = e.target.value;
-    localStorage.setItem('FORM_SAVED_INF', JSON.stringify(dataUpdate));      
-};
+    localStorage.setItem(FORM_SAVED_INF, JSON.stringify(dataUpdate));
+    console.log(dataUpdate);
+}
+
+function filledTextarea() {
+    const saveMessage = JSON.parse(localStorage.getItem(FORM_SAVED_INF));
+  if (saveMessage) {
+    console.log(saveMessage);
+    input.value = saveMessage.email;
+    textarea.value = saveMessage.message;
+  }
+}
 
 function formSubmit(e) {
     e.preventDefault();
-
-    const formActive = e.currentTarget.elements;
-    const inputActive = formActive.email.value;
-    const textareaActive = formActive.message.value;
-    const activeInf = {
-        inputActive,
-        textareaActive,
-    };
-    console.log(activeInf);
     e.currentTarget.reset();
     localStorage.removeItem(FORM_SAVED_INF);
-};
-
-filledTextarea();
-
-function filledTextarea() {
-    const savedMessage = localStorage.getItem(FORM_SAVED_INF);
-    const parsedMessage = JSON.parse(savedMessage);
-  
-    if (parsedMessage) {
-        input.value = parsedMessage.inputActive; 
-        textarea.value = parsedMessage.textareaActive;
-        dataUpdate.textareaActive = parsedMessage.textareaActive;
-        dataUpdate.inputActive = parsedMessage.inputActive;
-    }
-};
-
-
-
+}
 
 
